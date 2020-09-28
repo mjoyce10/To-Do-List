13e1af90d112c3f34aaa571aa402b073e3bcd160
Maureen Joyce <mjoyce10@kent.edu>
2020-09-27 21:48:59 -0400

    completed assignment

---
+++ List.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Choose_Your_Class
+{
+    public class List
+    {
+        public List<ToDo> ToDoList = new List<ToDo>();
+
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,49 @@ namespace Choose_Your_Class
     {
         static void Main(string[] args)
         {
-            
+            ToDo myToDoList = new ToDo();
+            Console.WriteLine("Hello! Welcome to the To-Do List App.");
+            bool usingToDoList = true;
+            while (usingToDoList)
+            {
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. See To-Do List.");
+                Console.WriteLine("2. Add a task.");
+                Console.WriteLine("3. Complete a task.");
+                Console.WriteLine("4. Remove a task.");
+                Console.WriteLine("5. Edit an urgency level.");
+                Console.WriteLine("6. Exit.");
+
+                string menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        myToDoList.PrintList();
+                        myToDoList.ShowMoreInfo();
+                        break;
+                    case "2":
+                        myToDoList.AddTask();
+                        break;
+                    case "3":
+                        myToDoList.CompleteTask();
+                        break;
+                    case "4":
+                        myToDoList.RemoveTask();
+                        break;
+                    case "5":
+                        myToDoList.ChangeUrgencyLevel();
+                        break;
+                    case "6":
+                        usingToDoList = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+                
+                Console.Clear();
+            }
         }
     }
 }
